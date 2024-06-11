e1b5551fd723de96db4a1d287642973456bb0510
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-10 23:17:55 -0300

    feat: add Spinner component

---
+++ src/components/shared/spinner.tsx
@@ -0,0 +1,14 @@
+export default function Spinner({ text, className }: SpinnerProps) {
+  return (
+    <div
+      className={`text-surface inline-block size-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white ${className}`}
+      role="status"
+    >
+      {text && (
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          {text}
+        </span>
+      )}
+    </div>
+  );
+}
