ef16ff1ef1d743c4534157b4ae8f38413e4772ca
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-10 23:16:56 -0300

    feat: add application types

---
+++ src/types/index.d.ts
@@ -0,0 +1,40 @@
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+declare type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+};
+
+declare type UserCardProps = {
+  user: User;
+  clickEvent: () => void;
+};
+
+declare type SpinnerProps = {
+  text?: string;
+  className?: string;
+};
+
