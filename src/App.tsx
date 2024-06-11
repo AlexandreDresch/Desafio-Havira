761744004d3310e5d37617b151865ac73c205987
AlexandreDresch <connectWithAlexandre@proton.me>
2024-06-10 23:20:33 -0300

    feat: add components to app & providers to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import Header from "./components/shared/header";
+import Layout from "./components/shared/layout";
+import UserMap from "./components/shared/user-map";
+
 export default function App() {
   return (
     <>
+      <Header />
+      <Layout>
+        <UserMap />
+      </Layout>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App";
+
+import { Provider } from "react-redux";
+import store from "./state/store";
+
+import { Toaster } from "./components/ui/toaster";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
   </React.StrictMode>,
-)
+);
